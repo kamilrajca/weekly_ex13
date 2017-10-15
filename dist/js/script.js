7b82db0475dfb949fcfaa2627f19cfe45da4a3ee
Kamil <kamil.rajca@gmail.com>
2017-10-16 00:28:30 +0200

    Initial dist subtree commit

---
+++ dist/js/script.js
@@ -0,0 +1,15 @@
+// płynne przewijanie menu
+     $('.menu-link').click(
+    function(event){
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 800)});
+
+// hamburger
+$(document).ready(function(){
+  $('.menu-toggler').click(function(e){
+    e.preventDefault();
+    $('.menu').toggleClass('visible');
+  });
+});
